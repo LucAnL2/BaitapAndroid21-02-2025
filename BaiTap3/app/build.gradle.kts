7845a678b3ebe5d08af1ab8302371bca46d21b8d
“LucAnL2” <huynhthaitoan.12c1htk@gmail.com>
2025-02-21 11:54:54 +0700

    hoan thanh bai tap 3

--- BaiTap3/app/build.gradle.kts
+++ BaiTap3/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.baitap3"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.baitap3"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
